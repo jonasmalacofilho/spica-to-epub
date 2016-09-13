631e9a12d30676055bbdc6393b78c07b206ffc00
Jonas Malaco <jonas@protocubo.io>
2016-09-13 19:52:40 -0300

    UI: better cli

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,18 @@
+import haxe.io.Path;
+import sys.FileSystem;
+import sys.io.File;
+
 class Main {
 	static function main()
 	{
 		try {
-			for (f in Sys.args()) {
-				var tex = Parser.parse(f);
-				sys.io.File.saveContent('$f.ast.json', haxe.Json.stringify(tex, "  "));
+			switch Sys.args() {
+			case [input, output]:
+				var tex = Parser.parse(input);
+				FileSystem.createDirectory(output);
+				File.saveContent(Path.join([output, "ast.json"]), haxe.Json.stringify(tex, "  "));
+			case other:
+				assert(false, other, "Usage: epubify <input-root> <output-dir>");
 			}
 		} catch (err:String) {
 			if (err != "Aborted")
